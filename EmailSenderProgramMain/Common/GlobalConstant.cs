5f8c497b56a59fc89c0542d0dba9d36ad4437c39
RamaKrishnanBE <g.ramakrishnan.be@gmail.com>
2025-04-17 12:31:56 +0530

    Remove unused files

--- EmailSenderProgramMain/Common/GlobalConstant.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace EmailSenderProgramMain.Common
-{
-    internal class GlobalConstant
-    {
-        public const string CONST_APPLICATION_STARTUP_WELCOME_MESSAGE = "Send Welcomemail";
-    }
-}
